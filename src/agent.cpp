e769d4135c208e8bcb5cbacfc06bace848a840c4
hruday <ramahrudayb@gmail.com>
2023-03-23 10:04:54 -0500

    basic classes for env and drone added

--- src/agent.cpp
+++ src/agent.cpp
@@ -7,41 +7,107 @@
 
 #include <commands_listener.hpp>
 
-int move_the_drone(int speed, int increment, int limit)
+/*
+NAME: Drone
+PURPOSE: To replicate RL Drone, i.e observe the agent and reward it accordingly
+INVARIANTS:
+*/
+
+class Drone
 {
 
-    int segments = 0;
-    ros::Rate rate(2);
-    while (ros::ok())
+private:
+    ros::Subscriber drone_pos_sub;       // subscriber to drone's position
+    geometry_msgs::Point drone_position; // geometry coordinates of the drone
+    std::int reward;
+
+public:
+    Drone(ros::NodeHandle nh, int[3] victim_position, int inc)
     {
-        ros::spinOnce();
-        rate.sleep();
-        if (check_waypoint_reached(.3) == 1)
+
+        // initialize control publisher/subscribers
+        init_publisher_subscriber(n, false);
+
+        // wait for FCU connection
+        wait4connect();
+
+        // wait for user to switch to mode GUIDED
+        wait4start();
+
+        // the intital position of drone is set as origin.
+        initialize_local_frame();
+
+        std::map<std::string, std::map<int, int>> QTable;
+        int action[3] = {0, 1, 2};
+
+        // drone position subscriber initiated
+        drone_pos_sub = nh.subscribe<nav_msgs::Odometry>("/mavros/global_position/local", 10,
+                                                         &Drone::pos_callback, this);
+        reward_sub = nh.subscribe<std::int>("/reward", 10,
+                                            &Drone::reward_callback, this);
+
+        // initiate random QTable
+        // iterate over possible states from victim position using inc
+        for (int i = 0; i < victim_position[0]; i += inc)
         {
-            std::vector<double> drone_pos = get_current_posing();
-
-            if (drone_pos[1] >= limit)
+            for (int j = 0; j < victim_position[1]; j += inc)
             {
-                break;
-            }
-
-            set_speed(speed);
-
-            for (size_t i = -1; i <= 1; i++)
-            {
-                if (check_waypoint_reached(.3) == 1)
+                for (int k in &Drone::action)
                 {
-                    set_destination(0, drone_pos[1], 2, i * 30);
+                    std::map<int, int> random_action_reward_map;
+                    int random_v = std::rand() % (3 + 1 - 1) + 1;
+                    random_action_reward_map.insert({k, random_v});
+                    QTable.insert({std::to_string(i) + std::to_string(j), random_action_reward_map});
                 }
             }
-
-            set_destination(0, segments + increment, 3, 0);
-            segments++;
         }
     }
 
-    return 0;
-}
+    /*
+     NAME: pos_callback
+     PARAMETERS: &msg , Pointer to the reading of Odometry channel
+     PURPOSE: prints the current position of drone {x,y,z}
+     PRECONDITION: the parameter should be a odometry reading
+     POSTCONDITION: the function updates the drone position in the Drone class and prints it out
+    */
+    void pos_callback(const nav_msgs::Odometry::ConstPtr &msg)
+    {
+        nav_msgs::Odometry odo_msg;
+        odo_msg = *msg;
+        drone_position = odo_msg.pose.pose.position;
+        ROS_INFO("co-ordinates pos d: %f %f %f", drone_position.x, drone_position.y, drone_position.z);
+    }
+
+    void reward_callback(const std::int &msg)
+    {
+        reward = *msg;
+        std::cout << reward;
+    }
+
+    /*
+     NAME: move_the_drone
+     PARAMETERS: speed (Speed of the drone), x, y, z (Coordinates)
+     PURPOSE: moves the drone to x,y,z point with velocity "speed m/s"
+     PRECONDITION: drone must be switched ON
+     POSTCONDITION: the function moves the drone to x,y,z point in the space with velocity "speed m/s"
+    */
+    void move_the_drone(int speed, int x, int y, int z)
+    {
+
+        ros::Rate rate(2);
+        while (ros::ok())
+        {
+            ros::spinOnce();
+            rate.sleep();
+            if (check_waypoint_reached(.3) == 1)
+            {
+                set_speed(speed);
+
+                set_destination(x, y, z, 0);
+            }
+        }
+    }
+};
 
 /*
     NAME: main
@@ -52,26 +118,15 @@ int move_the_drone(int speed, int increment, int limit)
 */
 int main(int argc, char **argv)
 {
+    // actions 0 -Left, 1 - Forward, 2- Right
+    int action[3] = {0, 1, 2};
+    int[3] initial_pos = {0, 0, 0};
+
     // initialize ros
     ros::init(argc, argv, "drone_nav");
     ros::NodeHandle n;
 
-    // initialize control publisher/subscribers
-    init_publisher_subscriber(n, false);
-
-    // wait for FCU connection
-    wait4connect();
-
-    // wait for user to switch to mode GUIDED
-    wait4start();
-
-    // the intital position of drone is set as origin.
-    initialize_local_frame();
-
-    // request drone to takeoff 3m high in air
-    takeoff(3);
-
-    move_the_drone(5, 5, 10);
+    Drone drone = Drone(nh, victim_pos, INCREMENT);
 
     wait4Land();
 }
--- src/env.cpp
+++ src/env.cpp
@@ -8,6 +8,7 @@
 #include <commands_listener.hpp>
 #include <std_msgs/Int64.h>
 #include <nav_msgs/Odometry.h>
+#include <cmath>
 
 /*
 NAME: Environment
@@ -19,15 +20,20 @@ class Environment
 {
 
 private:
-    ros::Subscriber drone_positon_subscriber; // a ros subsriber which subsribes to topic MAVROS_GLOBALPOSITION_LOCAL
-    geometry_msgs::Point drone_position;      // a geometry point of {x,y,z} i.e cordinates of the drone
+    ros::Subscriber drone_pos_sub;       // subscriber to drone's position
+    geometry_msgs::Point drone_position; // geometry coordinates of the drone
+
+    int[3] REWARDS = {-1, 0, 10};
 
 public:
-    Environment(ros::NodeHandle nh)
+    Environment(ros::NodeHandle nh, int[3] victim_position, int inc)
     {
-        // everytime a msg appears on the topic MAVROS_GLOBALPOSITION_LOCAL, pos_callback is called passing the msg. 10 is limit of msg to be qeued
-        drone_positon_subscriber = nh.subscribe<nav_msgs::Odometry>("/mavros/global_position/local", 10,
-                                                                    &Environment::pos_callback, this);
+        // drone position subscriber initiated
+        drone_pos_sub = nh.subscribe<nav_msgs::Odometry>("/mavros/global_position/local", 10,
+                                                         &Environment::pos_callback, this);
+
+        // publish reward here
+        reward_pub = controlnode.advertise<std::int>("/reward", 10);
     }
 
     /*
@@ -42,11 +48,32 @@ public:
         nav_msgs::Odometry odo_msg;
         odo_msg = *msg;
         drone_position = odo_msg.pose.pose.position;
-        ROS_INFO("cordinates pos d: %f %f %f", drone_position.x, drone_position.y, drone_position.z);
+        ROS_INFO("coordinates pos d: %f %f %f", drone_position.x, drone_position.y, drone_position.z);
+        double[2] polar = get_polar_coordinates();
+        if (polar[0] < 10)
+        {
+            reward_pub.publish(REWARDS[1])
+        }
+        else if (polar[0] > 20)
+        {
+            reward_pub.publish(REWARDS[0])
+        }
+    }
+
+    /*
+    NAME: get_polar_coordinates
+    PARAMETERS: no params
+    PURPOSE: to get polar coordinates with respect to person from the drone r,O
+    PRECONDITION: drone simulation and person must be standing in the gazebo world
+    POSTCONDITION: the function returns polar coordniates, later translated as state of the drone, no direct effect on the movement or navigation
+    */
+    double[2] get_polar_coordinates()
+    {
+        double r = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
+        double theta = asin(z / r);
+        return {r, theta};
     }
 };
-
-// command line or remapping arguments
 
 /*
     NAME: main
@@ -57,8 +84,12 @@ public:
 */
 int main(int argc, char **argv)
 {
+    int INCREMENT = 5;
     ros::init(argc, argv, "env");
     ros::NodeHandle nh;
-    Environment nc = Environment(nh);
+
+    int[3] victim_pos = {20, 20, 0} // x,y,z cordinates of the victim
+
+    Environment nc = Environment(nh, victim_pos, INCREMENT);
     ros::spin();
 }
